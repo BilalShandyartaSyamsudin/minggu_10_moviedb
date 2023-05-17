9ace41d765ed8a2e0a0c355309fa0a53ccd2ed9e
Bilal Shandyarta Syamsudin <e41212037@student.polije.ac.id>
2023-05-17 10:31:58 +0700

    in progres

---
+++ lib/data/api_provider.dart
@@ -0,0 +1,8 @@
+import 'dart:convert';
+import 'package:http/http.dart';
+import 'package:minggu_10_moviedb/model/popular_movie.dart';
+
+class ApiProvider{
+  String apiKey = '';
+  String baseUrl = 'https://api.themoviedb.org/'
+}
